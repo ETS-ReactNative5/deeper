d06b056b9ce1d705192287287ad3714a3064685c
nicolekwon <56002536+nicolekwon@users.noreply.github.com>
2022-02-09 17:30:10 -0500

    Adding a plus button to community tab

--- frontend-new/screens/community.js
+++ frontend-new/screens/community.js
@@ -8,7 +8,8 @@ import {
     Image,
     Dimensions,
     TextInput,
-    ScrollView
+    ScrollView,
+    Platform
 } from "react-native";
 
 import { COLORS, SIZES, FONTS } from "../constants";
@@ -18,25 +19,35 @@ const Community = () => {
     return (
         <View style={StyleSheet.container}>
             <SafeAreaView>
-                {/* Header */}
-                <View style={styles.headerWrapper}>
-                    <Text style={styles.headerTitle}>
+                <ScrollView>
+                    {/* Header */}
+                    <View style={styles.headerWrapper}>
+                        <Text style={styles.headerTitle}>
+                            <Text>
+                                Community Feed
+                            </Text>
+                        </Text>
+                    </View>
+                    {/* Search Bar */}
+                    <View style={styles.searchWrapper}>
+                        <Image source={require('../assets/icons/search.png')}
+                        style={styles.searchIcon}
+                        />
+                        <View style={styles.search}>
+                            <TextInput 
+                                style={styles.searchText}
+                                placeholder="Search" />
+                        </View>
+                    </View>
+                </ScrollView>
+                {/* Buttons */}
+                <TouchableOpacity style={styles.addButton}>
+                    <Text style={styles.addTitle}>
                         <Text>
-                            Community Feed
+                            +
                         </Text>
                     </Text>
-                </View>
-                {/* Search Bar */}
-                <View style={styles.searchWrapper}>
-                    <Image source={require('../assets/icons/search.png')}
-                    style={styles.searchIcon}
-                    />
-                    <View style={styles.search}>
-                        <TextInput 
-                            style={styles.searchText}
-                            placeholder="Search" />
-                    </View>
-                </View>
+                </TouchableOpacity>
             </SafeAreaView>
         </View>
     )
@@ -63,16 +74,16 @@ const styles = StyleSheet.create({
       },
       searchWrapper:
       {
-          flexDirection: 'row',
-          justifyContent: 'center',
-          alignItems: 'center',
-          width: Dimensions.get("window").width-40,
-          height: Dimensions.get("window").height/20,
-          marginVertical: 15,
-          marginHorizontal: 20,
-          backgroundColor: '#EAE9E9',
-          top: 5,
-          borderRadius: 10,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: Dimensions.get("window").width-40,
+        height: Dimensions.get("window").height/20,
+        marginVertical: 15,
+        marginHorizontal: 20,
+        backgroundColor: '#EAE9E9',
+        top: 5,
+        borderRadius: 10,
       },
       searchIcon: {
         width: 25,
@@ -82,13 +93,31 @@ const styles = StyleSheet.create({
       },
       search:
       {
-          flex: 1,
-          marginLeft: 15,
+        flex: 1,
+        marginLeft: 15,
       },
       searchText:
       {
-          fontFamily: 'Arial',
-          fontSize: 16,
-          color: '#747480',
+        fontFamily: 'Arial',
+        fontSize: 16,
+        color: '#747480',
+      },
+      addButton:
+      {
+        width: 60,
+        height: 60,
+        backgroundColor: '#FEC62F',
+        borderRadius: 100,
+        justifyContent: 'center',
+        position: 'absolute',
+        bottom: 0,
+        right: 15,
+      },
+      addTitle:
+      {
+        fontFamily: 'Avenir',
+        fontSize: 40,
+        bottom: 0, 
+        alignSelf: 'center',
       },
 })
